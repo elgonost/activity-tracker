3470f31f45286832c6eb201fe51198bbdbf2624c
elgonost <kostaspetsasx@gmail.com>
2016-06-05 16:18:35 +0300

    test

---
+++ src/app_message.c
@@ -0,0 +1,299 @@
+#include <pebble.h>
+
+#define BUFFER_SIZE 1300
+// declaration of the byte arrays we send
+// X,Y,Z are 4 digit numbers so we only need 2 concecutive bytes
+// to represent each one
+#define XYZ_BYTE_ARRAY_SIZE BUFFER_SIZE*2
+// Timestamp is considered 9 digit number for convenience so we use 5 concecutive bytes
+// to represent each timestamp value.
+#define TIME_BYTE_ARRAY_SIZE 14
+#define NUM_SAMPLES 25
+
+
+static Window *s_window;
+
+static int s_battery_level;
+static TextLayer *s_battery_layer;
+
+static TextLayer *s_time_layer;
+DictionaryIterator *iter;
+bool js_flag = false;
+bool data_col = true;
+bool activity_flag = false;
+
+//Byte arrays
+uint8_t XBytes[XYZ_BYTE_ARRAY_SIZE];
+uint8_t YBytes[XYZ_BYTE_ARRAY_SIZE];
+uint8_t ZBytes[XYZ_BYTE_ARRAY_SIZE];
+uint8_t TimeBytes[TIME_BYTE_ARRAY_SIZE];	
+
+int counter = 0;
+int timeCounter = 0;
+
+int act_index = 0;
+int select_counter = 0;
+int number_of_activities = 0;
+char* adl = "None";
+char* duration = "None";
+char* status = "Disabled";
+
+// Keys for AppMessage Dictionary
+// These should correspond to the values 
+//you defined in appinfo.json/Settings
+enum {
+	STATUS_KEY = 0,	
+	MESSAGE_KEY = 1,
+  X_KEY = 2,
+  Y_KEY = 3,
+  Z_KEY = 4,
+  TIME_KEY = 5,
+  TIME_VALUE_START = 6,
+  TIME_VALUE_END = 7,
+  ACTIVITY = 8,
+  ACTIVITY_NUMBER = 9
+};
+
+struct byteForm{
+  uint8_t first_half;
+  uint8_t second_half;
+};
+
+struct timeByteForm{
+  uint8_t byte_0;
+  uint8_t byte_1;
+  uint8_t byte_2;
+  uint8_t byte_3;
+  uint8_t byte_4;
+  
+  uint8_t byte_5;
+  uint8_t byte_6;
+};
+
+//Converts the accelerometer values to bytes
+struct byteForm convert_xyz_to_bytes(int value){
+  struct byteForm split;
+  
+  //OR operation with 128 is equal to making MSB = 1
+  //which represents the negative sign
+  if(value<0)
+    split.first_half = (abs(value)/100) | 128;
+  else
+    split.first_half = abs(value)/100;
+  split.second_half = abs(value)%100;
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "Test value: %d. Converted to %d%d",value,split.first_half,split.second_half);
+  return split;
+}
+
+struct timeByteForm convert_time_to_bytes(int value1,int value2){
+  struct timeByteForm split;
+  value2 = abs(value2);
+  value1 = abs(value1);
+  split.byte_0 = value2 % 100;
+  split.byte_1 = (value2/100) % 100;
+  split.byte_2 = (value2/10000) % 100;
+  split.byte_3 = (value2/1000000);
+  split.byte_4 = value1 % 100;
+  split.byte_5 = (value1/100) % 100;
+  split.byte_6 = (value1/10000);
+  return split;
+}
+
+struct byteForm temp;
+struct timeByteForm temp2,referenceTimestamp,choiceTimestamp;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+  
+}
+
+static void battery_callback(BatteryChargeState state) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG,"battery level is %d ",s_battery_level);
+  // Record the new battery level
+  s_battery_level = state.charge_percent;
+  // Update meter
+  //layer_mark_dirty(s_battery_layer);
+  static char s_buffer[128];
+   
+  // Compose string of all data
+  snprintf(s_buffer, sizeof(s_buffer),"Battery: %d%%",s_battery_level);
+  text_layer_set_text(s_battery_layer, s_buffer);
+}
+
+
+
+static void data_handler(AccelData *data, uint32_t num_samples) {
+    
+  if((js_flag)&&(data_col)){
+    //javascript is ready and we can start gathering data
+    if(counter<BUFFER_SIZE){
+      //keep gathering data
+      for(int i=0;i<NUM_SAMPLES;i++){
+        temp = convert_xyz_to_bytes(data[i].x);
+        XBytes[2*counter] = temp.first_half;  
+        XBytes[2*counter+1] = temp.second_half;
+        
+        temp = convert_xyz_to_bytes(data[i].y);
+        YBytes[2*counter] = temp.first_half;  
+        YBytes[2*counter+1] = temp.second_half;
+        
+        temp = convert_xyz_to_bytes(data[i].z);
+        ZBytes[2*counter] = temp.first_half;  
+        ZBytes[2*counter+1] = temp.second_half;
+        
+        if( ( (i==0) && counter==0 ) || ( ( i==(NUM_SAMPLES-1)) && (counter==(BUFFER_SIZE-1)) )) {
+          APP_LOG(APP_LOG_LEVEL_DEBUG,"writing timestamp");
+          APP_LOG(APP_LOG_LEVEL_DEBUG,"Counter=%d and i=%d",counter,i);
+          temp2 = convert_time_to_bytes(data[i].timestamp / 100000000,data[i].timestamp % 100000000);
+          
+          TimeBytes[timeCounter] = temp2.byte_6;
+          TimeBytes[timeCounter+1] = temp2.byte_5;
+          TimeBytes[timeCounter+2] = temp2.byte_4;
+          TimeBytes[timeCounter+3] = temp2.byte_3;
+          TimeBytes[timeCounter+4] = temp2.byte_2;
+          TimeBytes[timeCounter+5] = temp2.byte_1;
+          TimeBytes[timeCounter+6] = temp2.byte_0;
+          
+          timeCounter+=7;
+        }
+        counter++;
+      }
+      //for debugging
+      if(counter==325)
+        APP_LOG(APP_LOG_LEVEL_DEBUG,"Counter is 325");
+      if(counter==650)
+        APP_LOG(APP_LOG_LEVEL_DEBUG,"Counter is 650");
+      if(counter==975)
+        APP_LOG(APP_LOG_LEVEL_DEBUG,"Counter is 975");
+      if(counter==1300)
+        APP_LOG(APP_LOG_LEVEL_DEBUG,"Counter is 1300 . . . sending");
+      
+    }
+    else{
+      //When we get here our Buffers are full. We need to send data
+      //to PebbleKitJS.
+      app_message_outbox_begin(&iter);
+      dict_write_data(iter,X_KEY,&XBytes[0],sizeof(XBytes));
+      dict_write_data(iter,Y_KEY,&YBytes[0],sizeof(YBytes));
+      dict_write_data(iter,Z_KEY,&ZBytes[0],sizeof(ZBytes));
+      dict_write_data(iter,TIME_KEY,&TimeBytes[0],sizeof(TimeBytes));
+      dict_write_end(iter);
+      app_message_outbox_send();      
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Message was sent to PebbleKitJS"); 
+      counter = 0;
+      timeCounter = 0;
+    }
+  }
+}
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *tuple;
+	
+	tuple = dict_find(received, STATUS_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
+	}
+	
+	tuple = dict_find(received, MESSAGE_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
+	}
+  js_flag = true;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "JS_FLAG is set true");
+}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {	
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+}
+
+// Called when PebbleKitJS acknowledges receipt of a message
+static void out_sent_handler(DictionaryIterator *iter, void *context) {
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+
+  // Create output TextLayer
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 45), window_bounds.size.w, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+ 
+  s_battery_layer = text_layer_create(GRect(0, 90, window_bounds.size.w - 10, window_bounds.size.h-140));
+  text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text(s_battery_layer, "No data yet.");
+  text_layer_set_overflow_mode(s_battery_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
+    
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy output TextLayer
+  text_layer_destroy(s_time_layer);
+}
+
+static void init(void) {
+	s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });  
+  //window_set_click_config_provider(s_window, (ClickConfigProvider) config_provider);
+	window_stack_push(s_window, true);    
+  update_time();
+  battery_state_service_subscribe(battery_callback);
+  battery_callback(battery_state_service_peek());
+	
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+  //app_message_register_outbox_sent(out_sent_handler);
+
+  accel_data_service_subscribe(NUM_SAMPLES, data_handler);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
+  
+  // Initialize AppMessage inbox and outbox buffers with a suitable size
+  const int inbox_size = 128;
+  const int outbox_size = 8000;
+  app_message_open(inbox_size, outbox_size);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+}
+
+static void deinit(void) {
+	app_message_deregister_callbacks();
+	window_destroy(s_window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
---
+++ src/js/app.js
@@ -0,0 +1,268 @@
+var buffer_size = 1300;
+// Changes with sampling rate
+// 10 Hz : ms = 100 | 25 Hz : ms = 40 | 50 Hz : ms = 20 | 100 Hz : ms = 10
+//var ms = 40; 
+var time_samples = 2;
+var xarray = [];
+var yarray = [];
+var zarray = [];
+var timearray = [];
+var activityType = [];
+var typeStart = [];
+var typeEnd = [];
+var sendData = {};
+var username = localStorage.getItem("username");
+var numberOfMessages = 0;
+var request = new XMLHttpRequest();
+var possibleFall = false;
+var peakIndex = 0;
+var overlappingVectorSumLeft = [];
+var overlappingVectorSumRight = [];
+
+
+console.log("* * *Mobile phone says hello!!!* * *");
+console.log(username);
+
+function convertBytesToAccel(msB,lsB){
+  var temp = msB | 127;
+  if (temp!=255)
+    return (msB*100 + lsB);
+  else{
+    msB = msB & 127;
+    return -(msB*100 + lsB);
+  }  
+}
+
+function convertBytesToTime(byte6,byte5,byte4,byte3,byte2,byte1,byte0){
+  return (byte6*1000000000000 + byte5*10000000000 + byte4*100000000 + byte3*1000000 + byte2*10000 + byte1*100 + byte0);
+}
+
+// Function to send a message to the Pebble using AppMessage API
+// We are currently only sending a message using the "status" appKey defined in appinfo.json/Settings
+function sendMessage() {
+	Pebble.sendAppMessage({"status": 1}, messageSuccessHandler, messageFailureHandler);
+}
+
+// Called when the message send attempt succeeds
+function messageSuccessHandler() {
+  console.log("Message send succeeded.");  
+}
+
+// Called when the message send attempt fails
+function messageFailureHandler() {
+  console.log("Message send failed.");
+  sendMessage();
+}
+
+
+
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://e088a03dc67dacfb01333eea79f5192d2b79960c.googledrive.com/host/0By0ivjD6euU1ZDlBLWZ4YmFMT2s/form.html';
+  console.log('Showing configuration page: '+ url);
+  Pebble.openURL(url);
+});
+
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  // Decode the user's preferences
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  username = configData.userName;
+  localStorage.setItem("username",username);
+  console.log("username set: "+username);
+});
+
+
+
+// Called when JS is ready
+Pebble.addEventListener("ready", function(e) {
+  console.log("JS is ready!");
+  sendMessage();
+});
+						
+
+
+// Called when incoming message from the Pebble is received
+Pebble.addEventListener("appmessage", function(e) {
+  
+    var vectorSum = [];
+    var variance = 0;
+    var mean = 0;
+    var meanCos = 0;
+    var meanDistance = 0;
+    var tempMinimum = 0;
+    var fallDistance = 0;
+    
+    if(e.payload.timevalue !== undefined){
+      
+      numberOfMessages++;
+      for(var i=0;i<buffer_size;i++){
+       
+        xarray.push(convertBytesToAccel(e.payload.xvalue[2*i],e.payload.xvalue[2*i+1]));
+        yarray.push(convertBytesToAccel(e.payload.yvalue[2*i],e.payload.yvalue[2*i+1]));
+        zarray.push(convertBytesToAccel(e.payload.zvalue[2*i],e.payload.zvalue[2*i+1]));
+        vectorSum.push(  Math.sqrt(Math.pow(xarray[i],2)  +  Math.pow(yarray[i],2)  +  Math.pow(zarray[i],2))  );
+        mean += Math.sqrt(Math.pow(xarray[i],2)  +  Math.pow(yarray[i],2)  +  Math.pow(zarray[i],2));
+      }
+      
+    
+      
+      //**************************************************** fall detection ******************************************************************
+      if(possibleFall){
+        if(peakIndex+15>=buffer_size){
+          //auto simainei oti ola dedomena pou xreiazomaste einai
+          //ston trexonta pinaka kai oxi ston overlappingRight.
+          var m = peakIndex+15-buffer_size;
+          for(var o=m;o<m+35;o++)
+            fallDistance += Math.sqrt( Math.pow((xarray[o+1] - xarray[o]),2) + Math.pow((yarray[o+1] - yarray[o]),2) + Math.pow((zarray[o+1] - zarray[o]),2) );
+            fallDistance = fallDistance / 35;
+              console.log("fallDistance after impact was: "+fallDistance);
+              if(fallDistance<=85.91){
+                console.log("FALL DETECTED. CALL FOR HELP!!");
+              } 
+        }
+        else{
+          //edw exoume kapoia dedomena ston proigoumeno pinaka
+          //kai kapoia ston trexonta opote xreiazomaste ton overlapping
+          var tempo = buffer_size - (peakIndex+15);
+          for(var r=50-tempo;r<50;r++)
+            fallDistance += overlappingVectorSumRight[r];
+          for(r=0;r<35-tempo;r++)
+            fallDistance += Math.sqrt( Math.pow((xarray[r+1] - xarray[r]),2) + Math.pow((yarray[r+1] - yarray[r]),2) + Math.pow((zarray[r+1] - zarray[r]),2) );
+          fallDistance = fallDistance / 35;
+          console.log("fallDistance after impact was: "+fallDistance);
+          if(fallDistance<=85.91){
+            console.log("FALL DETECTED. CALL FOR HELP!!");
+          } 
+        }
+      possibleFall = false; 
+      }
+      
+      for(i=0;i<buffer_size;i++){
+        if(vectorSum[i]>=3874.88){
+          console.log("Possible Fall here cause of high vectorSum: "+vectorSum[i]);
+          //check if there is a minimum <= 697.28 before the peak
+          tempMinimum = vectorSum[i];
+          if(i-15<0){
+            //uses overlapping window 
+            for(var k=i;k<15;k++){
+              if (overlappingVectorSumLeft[k]<tempMinimum)
+                tempMinimum = overlappingVectorSumLeft[k];
+            }
+            for(k=0;k<i;k++){
+              if (vectorSum[k]<tempMinimum)
+                tempMinimum = vectorSum[k];
+            }
+          }
+          else{
+            for(var j=i-15;j<i;j++){
+              if (vectorSum[j]<tempMinimum)
+                tempMinimum=vectorSum[j];
+            }
+          }
+          console.log("minimum before peak was: "+tempMinimum);
+          
+          if(tempMinimum<=697.28){
+              //possible fall, check for sleep/mild activity at (i+15,i+50)
+            if(i+50>=buffer_size){   
+              overlappingVectorSumRight.length = 0;
+              for(var l=buffer_size-50;l<buffer_size;l++)
+                overlappingVectorSumRight.push(Math.sqrt(Math.pow(xarray[l],2)  +  Math.pow(yarray[l],2)  +  Math.pow(zarray[l],2)) );
+              possibleFall = true; //to check later
+              peakIndex = i;
+            }
+            else{
+              for(var p=i+15;p<i+50;p++)
+                fallDistance += Math.sqrt( Math.pow((xarray[p+1] - xarray[p]),2) + Math.pow((yarray[p+1] - yarray[p]),2) + Math.pow((zarray[p+1] - zarray[p]),2) );
+              fallDistance = fallDistance / 35;
+              console.log("fallDistance after impact was: "+fallDistance);
+              if(fallDistance<=85.91){
+                console.log("FALL DETECTED. CALL FOR HELP!!");
+              } 
+            }
+          }
+          
+           
+        }
+      }
+      
+      overlappingVectorSumLeft.length = 0;
+      //create overlapping window
+      for(i=buffer_size-15;i<buffer_size;i++){
+          overlappingVectorSumLeft.push(Math.sqrt(Math.pow(xarray[i],2)  +  Math.pow(yarray[i],2)  +  Math.pow(zarray[i],2)) );
+      }
+      
+      
+      //**************************************************** fall detection ******************************************************************
+      
+      for(i=0;i<time_samples;i++){
+        var temp = convertBytesToTime(e.payload.timevalue[7*i],e.payload.timevalue[7*i+1],e.payload.timevalue[7*i+2],
+                                  e.payload.timevalue[7*i+3],e.payload.timevalue[7*i+4],e.payload.timevalue[7*i+5],
+                                  e.payload.timevalue[7*i+6]);
+        console.log(temp);
+        timearray.push(temp); 
+      }
+      
+      mean = mean / buffer_size;
+      for(i=0;i<buffer_size;i++){
+        variance += Math.pow((vectorSum[i] - mean),2);
+      }
+      variance = variance / buffer_size;
+      for(i=0;i<buffer_size-1;i++){
+        meanCos += ( xarray[i]*xarray[i+1] + yarray[i]*yarray[i+1] + zarray[i]*zarray[i+1] ) / ( vectorSum[i] * vectorSum[i+1] );
+        meanDistance += Math.sqrt( Math.pow((xarray[i+1] - xarray[i]),2) + Math.pow((yarray[i+1] - yarray[i]),2) + Math.pow((zarray[i+1] - zarray[i]),2) );
+      }
+      meanCos = meanCos / (buffer_size-1);
+      meanDistance = meanDistance / (buffer_size-1);
+      
+      console.log("== meanDist: "+meanDistance+" == meanCos: "+meanCos+" == variance: "+variance);
+      if(meanDistance <= 85.91){
+        if(meanCos <= 0.99963){
+          console.log("++++++++++++ MILD ++++++++++++");
+          activityType.push("Mild");
+          typeStart.push(timearray[0]);
+          typeEnd.push(timearray[timearray.length-1]);
+        }
+        else{
+          console.log("zzzzzzzZzZzZ SLEEP zzZZzZzZzZZZz");
+          activityType.push("Sleep");
+          typeStart.push(timearray[0]);
+          typeEnd.push(timearray[timearray.length-1]);
+        }
+      }
+      else{
+        if(variance <= 233345.72){
+          console.log("------------ MODERATE ---------------");
+          activityType.push("Moderate");
+          typeStart.push(timearray[0]);
+          typeEnd.push(timearray[timearray.length-1]);
+        }
+        else{
+          console.log("^^^^^^^^^^^^ INTENSE ^^^^^^^^^^^^^^^^");
+          activityType.push("Intense");
+          typeStart.push(timearray[0]);
+          typeEnd.push(timearray[timearray.length-1]);
+        }
+      }
+      
+      xarray.length = 0;
+      yarray.length = 0;
+      zarray.length = 0;
+      timearray.length = 0;
+      
+      if(numberOfMessages==120){
+        
+        sendData = {"user":username,"type":activityType,"start":typeStart,"end":typeEnd,"startTime":typeStart[0],"endTime":typeEnd[typeEnd.length-1]};     
+        
+        request.open('POST','http://83.212.115.163:8080/tracker');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');   
+        request.send(JSON.stringify(sendData));
+         
+        numberOfMessages = 0;
+        activityType.length = 0;
+        typeStart.length = 0;
+        typeEnd.length = 0;
+      }
+      
+    }
+        
+});
